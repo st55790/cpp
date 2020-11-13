c1ef1d82633b234a905011bd44de29c23f2e211c
Tomas Prudky <64365934+st55790@users.noreply.github.com>
2020-11-13 17:37:56 +0100

    some error

--- Exercises07/Project/Project/Main.cpp
+++ Exercises07/Project/Project/Main.cpp
@@ -4,6 +4,10 @@
 #include "Address.h"
 #include "Person.h"
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 void Save();
 void Load();
 
@@ -11,6 +15,17 @@ int main() {
 
 	Save();
 	Load();
+
+	//Memory LEAK Check start
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+	_CrtDumpMemoryLeaks();
+	//Memory Leak Check end
+
 	return 0;
 }
 
